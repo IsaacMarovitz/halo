fc0deb69bbe70bfaac341c46a6bcb2f2feda4a10
Isaac Marovitz <isaacryu@icloud.com>
2025-10-07 16:52:16 -0400

    More theme cleanup
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- src/theme.rs
+++ src/theme.rs
@@ -1,12 +1,10 @@
 use iced::application::Appearance;
-use iced::border::Radius;
 use iced::widget::button::Status;
 use iced::widget::pane_grid::Highlight;
 use iced::widget::scrollable::Rail;
 use iced::widget::{button, checkbox, container, pane_grid, scrollable, text, text_editor};
-use iced::{Border, Color, application};
+use iced::{application, Border, Color};
 use std::default::Default;
-//const OFF_WHITE: Color = Color::from_rgb8(242, 239, 233);
 
 struct Palette {
     pub base: Color,
@@ -29,10 +27,10 @@ impl Default for Palette {
             base_darker: Color::from_rgb8(38, 62, 99),        // DARK BLUE
             base_darkest: Color::from_rgb8(22, 30, 59),       // DARK BLUE
             base_lighter: Color::from_rgb8(113, 134, 162),    // LIGHTER BLUE
-            base_lightest: Color::from_rgb8(158, 175, 195),   // LIGHTEST BLUE,
+            base_lightest: Color::from_rgb8(158, 175, 195),   // LIGHTEST BLUE
             background: Color::from_rgb8(12, 12, 30),         // DARKEST BLUE
             text: Color::from_rgb8(226, 226, 226),            // LIGHT GREY
-            disabled: Color::from_rgb8(153, 158, 162),        // MEDIUM GREY,
+            disabled: Color::from_rgb8(153, 158, 162),        // MEDIUM GREY
             accent: Color::from_rgb8(235, 94, 85),            // SALMON
             accent_secondary: Color::from_rgb8(255, 159, 28), // GOLD
             error: Color::from_rgb8(255, 77, 77),             // ERROR RED
@@ -40,7 +38,7 @@ impl Default for Palette {
     }
 }
 
-//borders
+// Borders
 const BORDER_RADIUS: f32 = 5.0;
 const BORDER_WIDTH: f32 = 2.0;
 
@@ -64,7 +62,7 @@ pub enum Theme {
 impl Theme {
     fn palette(&self) -> Palette {
         match self {
-            //TODO light palette
+            // TODO: Light palette
             Theme::Light => Palette::default(),
             Theme::Dark => Palette::default(),
         }
@@ -237,36 +235,30 @@ impl checkbox::Catalog for Theme {
     fn style(&self, _class: &Self::Class<'_>, status: checkbox::Status) -> checkbox::Style {
         let palette = self.palette();
 
+        let base = checkbox::Style {
+            background: palette.base.into(),
+            icon_color: palette.text,
+            border: Border {
+                radius: 2.0.into(),
+                width: 0.0,
+                color: Default::default(),
+            },
+            text_color: Some(palette.text),
+        };
+
         match status {
-            checkbox::Status::Active { .. } => checkbox::Style {
-                background: palette.base.into(),
-                icon_color: palette.text,
-                border: Border {
-                    radius: 2.0.into(),
-                    width: 0.0,
-                    color: Default::default(),
-                },
-                text_color: Some(palette.text),
+            checkbox::Status::Active { .. } => base,
+            checkbox::Status::Hovered { .. } => {
+                let mut hovered = base.clone();
+                hovered.background = palette.base_lighter.into();
+
+                hovered
             },
-            checkbox::Status::Hovered { .. } => checkbox::Style {
-                background: palette.base_lighter.into(),
-                icon_color: palette.text,
-                border: Border {
-                    radius: 2.0.into(),
-                    width: 0.0,
-                    color: Default::default(),
-                },
-                text_color: Some(palette.text),
-            },
-            checkbox::Status::Disabled { .. } => checkbox::Style {
-                background: palette.disabled.into(),
-                icon_color: palette.text,
-                border: Border {
-                    radius: 2.0.into(),
-                    width: 0.0,
-                    color: Default::default(),
-                },
-                text_color: Some(palette.text),
+            checkbox::Status::Disabled { .. } => {
+                let mut disabled = base.clone();
+                disabled.background = palette.disabled.into();
+
+                disabled
             },
         }
     }
@@ -282,55 +274,24 @@ impl text_editor::Catalog for Theme {
     fn style(&self, _class: &Self::Class<'_>, status: text_editor::Status) -> text_editor::Style {
         let palette = self.palette();
 
+        let style = text_editor::Style {
+            background: palette.background.into(),
+            border: Border {
+                radius: Default::default(),
+                width: 0.0,
+                color: Default::default(),
+            },
+            icon: palette.text,
+            placeholder: palette.disabled,
+            value: palette.text,
+            selection: palette.base_lighter,
+        };
+
         match status {
-            text_editor::Status::Active => text_editor::Style {
-                background: palette.background.into(),
-                border: Border {
-                    radius: Default::default(),
-                    width: 0.0,
-                    color: Default::default(),
-                },
-                icon: palette.text,
-                placeholder: palette.disabled,
-                value: palette.text,
-                selection: palette.base_lighter,
-            },
-            text_editor::Status::Hovered => text_editor::Style {
-                background: palette.background.into(),
-                border: Border {
-                    radius: Default::default(),
-                    width: 0.0,
-                    color: Default::default(),
-                },
-                icon: palette.text,
-                placeholder: palette.disabled,
-                value: palette.text,
-                selection: palette.base_lighter,
-            },
-            text_editor::Status::Focused => text_editor::Style {
-                background: self.palette().background.into(),
-                border: Border {
-                    radius: Default::default(),
-                    width: 0.0,
-                    color: Default::default(),
-                },
-                icon: palette.text,
-                placeholder: palette.disabled,
-                value: palette.text,
-                selection: palette.base_lighter,
-            },
-            text_editor::Status::Disabled => text_editor::Style {
-                background: Color::TRANSPARENT.into(),
-                border: Border {
-                    radius: Default::default(),
-                    width: 0.0,
-                    color: Default::default(),
-                },
-                icon: palette.text,
-                placeholder: palette.disabled,
-                value: palette.text,
-                selection: palette.base_lighter,
-            },
+            text_editor::Status::Active => style,
+            text_editor::Status::Hovered => style,
+            text_editor::Status::Focused => style,
+            text_editor::Status::Disabled => style,
         }
     }
 }
@@ -345,40 +306,19 @@ impl scrollable::Catalog for Theme {
     fn style(&self, _class: &Self::Class<'_>, _status: scrollable::Status) -> scrollable::Style {
         let palette = self.palette();
 
+        let rail = Rail {
+            background: Some(palette.base_darker.into()),
+            scroller: scrollable::Scroller {
+                color: palette.error,
+                border: Border::default(),
+            },
+            border: Border::default(),
+        };
+
         scrollable::Style {
             container: Default::default(),
-            vertical_rail: Rail {
-                background: Some(palette.base_darker.into()),
-                scroller: scrollable::Scroller {
-                    color: palette.error,
-                    border: Border {
-                        color: Color::default(),
-                        width: 0.0,
-                        radius: Radius::default(),
-                    },
-                },
-                border: Border {
-                    color: Color::default(),
-                    width: 0.0,
-                    radius: Radius::default(),
-                },
-            },
-            horizontal_rail: Rail {
-                background: Some(palette.base_darker.into()),
-                scroller: scrollable::Scroller {
-                    color: palette.error,
-                    border: Border {
-                        color: Color::default(),
-                        width: 0.0,
-                        radius: Radius::default(),
-                    },
-                },
-                border: Border {
-                    color: Color::default(),
-                    width: 0.0,
-                    radius: Radius::default(),
-                },
-            },
+            vertical_rail: rail,
+            horizontal_rail: rail,
             gap: None,
         }
     }
